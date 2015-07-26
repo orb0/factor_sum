0696ee1a028d43f8dfb1e45e37f37333be115d7c
Aubrey Stark-Toller <aubrey@stark.engineering>
2015-07-26 18:30:27 +0100

    Updated setup.py classifiers and version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='factor_sum',
-    version='0.1',
+    version='1.1',
     description='Sums positive integers that are both less than a user \
                  defined maximum and divisable by at least one number in a \
                  user defined set of numbers.',
@@ -11,11 +11,14 @@ setup(
     license='GPL3',
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GPL3 License',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 2.7',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+
     ],
     keywords='project django',
     packages=find_packages(exclude=['tests*']),
